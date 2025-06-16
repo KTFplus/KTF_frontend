c022e6ebbdff660ee3a3947db87564ba3098b566
Lynn <165683192+UReWUI@users.noreply.github.com>
2025-06-16 22:15:29 +0900

    chore: change text from '발음 평가 시작하기' to '발음 평가'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
                   className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-zinc-800 rounded-xl hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2 focus:ring-offset-gray-900 transform hover:scale-105 transition-all duration-300 shadow-lg hover:shadow-xl"
                 >
                   <AcademicCapIcon className="w-5 h-5 mr-2 text-gray-400" />
-                  발음 평가 시작하기
+                  발음 평가
                 </Link>
               </div>
               
